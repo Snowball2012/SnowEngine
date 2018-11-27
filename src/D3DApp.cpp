7440e41cb82cad69a5ca9d1147495d7982baaa1a
Snowball <snowball@live.ru>
2018-11-27 12:04:11 +0300

    integrate with free version of PVS-Studio

--- src/D3DApp.cpp
+++ src/D3DApp.cpp
@@ -1,3 +1,5 @@
+// This is an independent project of an individual developer. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++, C#, and Java: http://www.viva64.com
 //***************************************************************************************
 // d3dApp.cpp by Frank Luna (C) 2015 All Rights Reserved.
 //***************************************************************************************
