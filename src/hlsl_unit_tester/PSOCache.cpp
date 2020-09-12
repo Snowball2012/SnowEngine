f90c28c564288242d33a8876c82c6ebec74453d3
Snowball <snowball@live.ru>
2020-09-12 23:22:26 +0300

    pso cache - async compilation, hash calculations

--- src/hlsl_unit_tester/PSOCache.cpp
+++ src/hlsl_unit_tester/PSOCache.cpp
@@ -6,27 +6,92 @@
 #include "PSOCache.h"
 
 
+PSOCache::PSOCache(
+	ComPtr<ID3D12Device> device, CComPtr<IDxcCompiler3> compiler,
+	CComPtr<IDxcIncludeHandler> include_handler )
+	: m_device( std::move( device ) ), m_compiler( std::move( compiler ) )
+	, m_include_handler( std::move( include_handler ) )
+{
+}
+
+
 PSOCache::CompilationResult PSOCache::CompilePSO( CompilationRequest request )
 {
-	// placeholder
+	CompilationResult res;
 
-	SE_ENSURE( m_device );
-	SE_ENSURE( request.rootsig );
+	if ( ! CheckInnerState() )
+		return res;
 
-	// Calc hash, search for pso in a LUT. Compile a new PSO if not found.
-	// ToDo: async compilation
+	const PsoHash hash = CalcHash( request );
+	if ( auto it = m_pso_lut.find( hash ); it != m_pso_lut.end() )
+		return it->second;
 
-	std::promise<ComPtr<ID3D12PipelineState>> compilation_promise;
+	
 
-	ComPtr<ID3D12PipelineState> pso;
+	auto async_res = std::async([&, request]()
+	{
+		ComPtr<ID3D12PipelineState> pso;
 
-	D3D12_COMPUTE_PIPELINE_STATE_DESC pso_desc{};
-	pso_desc.pRootSignature = request.rootsig;
+		D3D12_COMPUTE_PIPELINE_STATE_DESC pso_desc{};
+		pso_desc.pRootSignature = request.rootsig;
 
-	if ( FAILED( m_device->CreateComputePipelineState( &pso_desc, IID_PPV_ARGS( &pso ) ) ) )
-		compilation_promise.set_value( nullptr );
-	else
-		compilation_promise.set_value( pso );
+		// ToDo: shader compilation
 
-	return CompilationResult( compilation_promise.get_future() );
+		DxcBuffer src_buffer;
+		src_buffer.Ptr = request.shader_text.data();
+		src_buffer.Size = request.shader_text.size();
+		src_buffer.Encoding = DXC_CP_ACP; // ansi
+
+		LPCWSTR args[] =
+		{
+			L"/T",
+			L"cs_6_1",
+			L"/Vd",
+			L"/default-linkage",
+			L"external",
+		};
+
+		m_compiler->Compile( &src_buffer, )
+
+		if ( FAILED( m_device->CreateComputePipelineState( &pso_desc, IID_PPV_ARGS( &pso ) ) ) )
+			pso = nullptr;
+
+		return pso;
+	});	
+
+	res = async_res.share();
+
+	m_pso_lut.insert( std::make_pair( CalcHash( request ), res ) );
+
+	return res;
+}
+
+
+namespace
+{
+	template<typename T> void HashCombine( uint64_t& seed, const T& val )
+	{
+		seed ^= std::hash<T>()( val ) + 0x9e3779b9 + ( seed << 6 ) + ( seed >> 2 );
+	}
+	
+	template<typename... Types>
+	uint64_t HashCombine ( const Types&... args )
+	{ 
+		uint64_t seed = 0;
+		( HashCombine( seed, args ) , ... );
+		
+		return seed;
+	}
+}
+
+
+bool PSOCache::CheckInnerState() const
+{
+	return SE_ENSURE( m_device )
+		&& SE_ENSURE( m_compiler );
+}
+
+PSOCache::PsoHash PSOCache::CalcHash( const CompilationRequest& request )
+{
+	return HashCombine( request.shader_text, request.preprocessor_directives, request.rootsig );
 }
--- src/hlsl_unit_tester/PSOCache.h
+++ src/hlsl_unit_tester/PSOCache.h
@@ -8,29 +8,43 @@
 #include <d3d12.h>
 
 #include <future>
-#include <string>
+#include <string_view>
 
 
 // Uses shader text hashes. Beware of collisions
 class PSOCache
 {
 public:
-	using PsoId = uint64_t;
+
 	struct CompilationRequest
 	{
 		std::string shader_text;
+		std::string preprocessor_directives; // an argument to dxc
 		ID3D12RootSignature* rootsig;
 	};
 
 	using CompilationResult = std::shared_future<ComPtr<ID3D12PipelineState>>;
 
 private:
+
 	using PsoHash = uint64_t;
 	std::unordered_map<PsoHash, CompilationResult> m_pso_lut;
+
 	ComPtr<ID3D12Device> m_device;
+	CComPtr<IDxcCompiler3> m_compiler;
+	CComPtr<IDxcIncludeHandler> m_include_handler;
 
 public:
+
+	PSOCache(
+		ComPtr<ID3D12Device> device, CComPtr<IDxcCompiler3> compiler,
+		CComPtr<IDxcIncludeHandler> include_handler );
+
 	CompilationResult CompilePSO( CompilationRequest request );
 
 private:
+
+	bool CheckInnerState() const; // Checks class invariants. Use in all public methods before doing anything else
+
+	static PsoHash CalcHash( const CompilationRequest& request );
 };
