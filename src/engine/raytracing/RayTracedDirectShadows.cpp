5cbeeecb34f36ed21f8b8cbeb1b71085e5e52846
Snowball <snowball@live.ru>
2022-04-08 23:19:41 +0300

    add some boilerplate for raytraced shadows

---
+++ src/engine/raytracing/RayTracedDirectShadows.cpp
@@ -0,0 +1,23 @@
+﻿#include <engine/stdafx.h>
+
+#include "RayTracedDirectShadows.h"
+
+inline GenerateRaytracedShadowmaskPass::GenerateRaytracedShadowmaskPass()
+{
+}
+
+inline void GenerateRaytracedShadowmaskPass::Draw(const Context& context, IGraphicsCommandList& cmd_list)
+{
+    D3D12_DISPATCH_RAYS_DESC dispatch_rays_desc;
+    dispatch_rays_desc.Depth = 1;
+    dispatch_rays_desc.Width = 512;
+    dispatch_rays_desc.Height = 512;
+    auto empty_arg = D3D12_GPU_VIRTUAL_ADDRESS_RANGE_AND_STRIDE{{}, {}, {}};
+    dispatch_rays_desc.CallableShaderTable = empty_arg;
+    dispatch_rays_desc.HitGroupTable = empty_arg;
+    dispatch_rays_desc.MissShaderTable = empty_arg;
+    dispatch_rays_desc.RayGenerationShaderRecord = D3D12_GPU_VIRTUAL_ADDRESS_RANGE{{},{}};
+    
+    cmd_list.DispatchRays(&dispatch_rays_desc);
+    NOTIMPL;
+}
---
+++ src/engine/raytracing/RayTracedDirectShadows.h
@@ -0,0 +1,49 @@
+﻿#pragma once
+
+#include <engine/FramegraphResource.h>
+
+class GPUDevice;
+
+class GenerateRaytracedShadowmaskPass
+{
+public:
+
+    struct Context
+    {
+    };
+
+    GenerateRaytracedShadowmaskPass();
+
+    void Draw(const Context& context, IGraphicsCommandList& cmd_list);    
+};
+
+template<class Framegraph>
+class GenerateRaytracedShadowmaskNode : public BaseRenderNode<Framegraph>
+{
+public:
+    using OpenRes = std::tuple<>;
+    using WriteRes = std::tuple<>;
+    using ReadRes = std::tuple<>;
+    using CloseRes = std::tuple<>;
+    
+private:
+    
+public:
+
+    virtual void Run(Framegraph& framegraph, IGraphicsCommandList& cmd_list) override;
+};
+
+template <class Framegraph>
+void GenerateRaytracedShadowmaskNode<Framegraph>::Run(Framegraph& framegraph, IGraphicsCommandList& cmd_list)
+{
+    OPTICK_EVENT();
+    
+    PIXBeginEvent(&cmd_list, PIX_COLOR( 200, 210, 230 ), "Raytraced Direct Shadows");
+
+    GenerateRaytracedShadowmaskPass::Context ctx;
+    GenerateRaytracedShadowmaskPass pass;
+
+    pass.Draw(ctx, cmd_list);
+    
+    PIXEndEvent(&cmd_list);
+}
